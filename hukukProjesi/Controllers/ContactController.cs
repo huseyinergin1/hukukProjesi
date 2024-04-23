ae7024de3c265a776ae07817dc648bab4d012dff
Hüseyin Ergin <saykomaniax123@hotmail.com>
2024-04-24 00:01:18 +0300

    v1
    ASP NET MVC, MSSQL ve hazır template kullanılarak yapılan bir hukuk sitesidir içerisinde blog paylaşımıda yapılabilir.

---
+++ hukukProjesi/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hukukProjesi.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Hukuk | İletişim";
+            return View();
+        }
+    }
+}
